0dc2129e1dca4cf92993d1f508f1332f5a632a68
Deep <tarabulsi.berlin@gmail.com>
2017-05-05 19:44:05 +0200

    Out.print without  new line Out.print with new line

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("Hallo World");
+	System.out.println("Hallo World");
+	System.out.print("Hallo World 2");
     }
 }
